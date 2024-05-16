b0334edeba5bd6348f495d39e7412f4e4d03a3c5
Jinpeng Shi <login@jpshi.me>
2024-05-16 11:32:09 +0800

    re: update database

--- lua/mindmap/database.lua
+++ lua/mindmap/database.lua
@@ -1,58 +1,33 @@
 local prototype = require("mindmap.prototype")
-local excerpt = require("mindmap.excerpt")
 local mindmap = require("mindmap.mindmap")
-local misc = require("mindmap.misc")
 
 local M = {}
 
 --------------------
--- Class Dababase
+-- Class Database
 --------------------
 
----@class Database : SimpleDatabase
----@field mindmaps SimpleDatabase All mindmaps.
----@field unused_excerpts SimpleDatabase Used excerpts.
-M.Database = prototype.SimpleDatabase:new({
-	mindmaps = prototype.SimpleDatabase:new(),
-	unused_excerpts = prototype.SimpleDatabase:new(),
-})
+---@class Database : SimpleItem
+M.Database = prototype.SimpleItem:new()
 
 ----------
 -- Instance Method
 ----------
 
----@param obj table?
+---@param tbl table?
 ---@return table
-function M.Database:new(obj)
-	obj = obj or {}
+function M.Database:new(tbl)
+	tbl = tbl or {}
+	tbl.type = "database"
+	tbl = prototype.SimpleItem:new(tbl, mindmap.Mindmap)
 
-	obj.id = obj.id or ("db-" .. misc.get_unique_id())
-	obj.type = obj.type or "db"
-	obj.created_at = obj.created_at or tonumber(os.time())
-	obj.updated_at = obj.updated_at or tonumber(os.time())
+	tbl.created_at = tbl.created_at or tonumber(os.time())
+	tbl.updated_at = tbl.updated_at or tonumber(os.time())
 
-	obj.mindmaps = obj.mindmaps or self.mindmaps
-	if obj.mindmaps then
-		for k, v in pairs(obj.mindmaps) do
-			if type(k) == "string" and type(v) == "table" then
-				obj.mindmaps[k] = mindmap.Mindmap:new(v)
-			end
-		end
-	end
-
-	obj.unused_excerpts = obj.unused_excerpts or self.unused_excerpts
-	if obj.unused_excerpts then
-		for k, v in pairs(obj.unused_excerpts) do
-			if type(k) == "string" and type(v) == "table" then
-				obj.unused_excerpts[k] = excerpt.Excerpt:new(v)
-			end
-		end
-	end
-
-	setmetatable(obj, self)
+	setmetatable(tbl, self)
 	self.__index = self
 
-	return obj
+	return tbl
 end
 
 ----------
