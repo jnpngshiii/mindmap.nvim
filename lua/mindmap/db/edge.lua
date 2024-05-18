4940502541e64e7e6dc13f43be3490ed43927ec7
Jinpeng Shi <login@jpshi.me>
2024-05-18 08:47:32 +0800

    db: add node and edge

---
+++ lua/mindmap/db/edge.lua
@@ -0,0 +1,93 @@
+local misc = require("mindmap.misc")
+
+---@class Edge
+---
+---@field id string Edge ID.
+---@field type string Edge type. Default: "edge". This field is reserved for future use.
+---
+---@field created_at integer Edge created time.
+---@field updated_at integer Space repetition updated time of the edge.
+---@field due_at integer Space repetition due time of the edge.
+---@field ease integer Space repetition ease of the edge.
+---@field interval integer Space repetition interval of the edge.
+---
+---@field from_node_id string "From" node ID of this edge.
+---@field to_node_id string "To" node ID of this edge.
+local Edge = {}
+
+--------------------
+-- Instance Method
+--------------------
+
+---Create a new edge.
+---@param id? string Edge ID.
+---@param type? string Edge type. Default: "edge". This field is reserved for future use.
+---@param created_at? integer Edge created time.
+---@param updated_at? integer Space repetition updated time of the edge.
+---@param due_at? integer Space repetition due time of the edge.
+---@param ease? integer Space repetition ease of the edge.
+---@param interval? integer Space repetition interval of the edge.
+---@param from_node_id? string "From" node ID of this edge.
+---@param to_node_id? string "To" node ID of this edge.
+function Edge:new(id, type, created_at, updated_at, due_at, ease, interval, from_node_id, to_node_id)
+	local edge = {
+		id = id or misc.get_unique_id(),
+		type = type or "edge",
+
+		created_at = created_at or tonumber(os.time()),
+		updated_at = updated_at or tonumber(os.time()),
+		due_at = due_at or 0,
+		ease = ease or 250,
+		interval = interval or 1,
+
+		to_node_id = to_node_id or "",
+		from_node_id = from_node_id or "",
+	}
+
+	setmetatable(edge, Edge)
+	self.__index = self
+
+	return edge
+end
+
+--------------------
+-- Class Method
+--------------------
+
+---Convert edge to table.
+---@param edge Edge Edge object to convert.
+---@return table
+function Edge.to_table(edge)
+	return {
+		id = edge.id,
+		type = edge.type,
+		created_at = edge.created_at,
+		updated_at = edge.updated_at,
+		due_at = edge.due_at,
+		ease = edge.ease,
+		interval = edge.interval,
+		to_node_id = edge.to_node_id,
+		from_node_id = edge.from_node_id,
+	}
+end
+
+---Convert table to edge.
+---@param table table Table to convert.
+---@return Edge
+function Edge.from_table(table)
+	return Edge:new(
+		table.id,
+		table.type,
+		table.created_at,
+		table.updated_at,
+		table.due_at,
+		table.ease,
+		table.interval,
+		table.to_node_id,
+		table.from_node_id
+	)
+end
+
+--------------------
+
+return Edge
---
+++ lua/mindmap/db/node.lua
@@ -0,0 +1,80 @@
+local misc = require("mindmap.misc")
+
+---@class Node
+---
+---@field id string Node ID.
+---@field type string Node type. Default: "node". This field is reserved for future use.
+---
+---@field created_at integer Node created time.
+---
+---@field from_edge_ids string[] Edge IDs from this node.
+---@field to_edge_ids string[] Edge IDs to this node.
+local Node = {}
+
+--------------------
+-- Instance Method
+--------------------
+
+---Create a new node.
+---@param id? string Node ID.
+---@param type? string Node type. Default: "node". This field is reserved for future use.
+---@param created_at? integer Node created time.
+---@param from_edge_ids? string[] Edge IDs from this node.
+---@param to_edge_ids? string[] Edge IDs to this node.
+function Node:new(id, type, created_at, from_edge_ids, to_edge_ids)
+	local node = {
+		id = id or misc.get_unique_id(),
+		type = type or "node",
+
+		created_at = created_at or tonumber(os.time()),
+
+		from_edge_ids = from_edge_ids or {},
+		to_edge_ids = to_edge_ids or {},
+	}
+
+	setmetatable(node, Node)
+	self.__index = self
+
+	return node
+end
+
+---Add "from" edge to node.
+---@param from_edge_id string "From" edge to add.
+function Node:add_from_edge(from_edge_id)
+	table.insert(self.from_edge_ids, from_edge_id)
+end
+
+---Add "to" edge to node.
+---@param to_edge_id string "To" edge ID to add.
+---@return nil
+function Node:add_to_edge(to_edge_id)
+	table.insert(self.to_edge_ids, to_edge_id)
+end
+
+--------------------
+-- Class Method
+--------------------
+
+---Convert node to table.
+---@param node Node Node to convert.
+---@return table
+function Node.to_table(node)
+	return {
+		id = node.id,
+		type = node.type,
+		created_at = node.created_at,
+		from_edge_ids = node.from_edge_ids,
+		to_edge_ids = node.to_edge_ids,
+	}
+end
+
+---Convert table to node.
+---@param table table Table to convert.
+---@return Node
+function Node.from_table(table)
+	return Node:new(table.id, table.type, table.created_at, table.from_edge_ids, table.to_edge_ids)
+end
+
+--------------------
+
+return Node
