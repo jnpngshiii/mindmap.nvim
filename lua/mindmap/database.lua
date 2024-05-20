78450560cc595af21070217c93c173a96a410a0b
Jinpeng Shi <login@jpshi.me>
2024-05-21 05:48:17 +0800

    feat: add class Database

---
+++ lua/mindmap/database.lua
@@ -0,0 +1,49 @@
+local graph_class = require("mindmap.graph")
+local misc = require("mindmap.misc")
+
+---@alias path string
+
+---@class Database
+---@field cache table<path, Graph> Cache of graphs in different repos.
+local Database = {}
+
+function Database:new()
+	local database = {
+		cache = {},
+	}
+
+	setmetatable(database, self)
+	self.__index = self
+
+	return database
+end
+
+---Add a graph to the database.
+---@param path? path Path to load and save the graph.
+---@param log_level? string Logger log level of the graph.
+---@param show_log_in_nvim? boolean Show log in Neovim when added.
+---@param save_path? string Path to load and save the graph.
+function Database:add_graph(path, log_level, show_log_in_nvim, save_path)
+	local graph = graph_class["Graph"]:new(log_level, show_log_in_nvim, save_path)
+
+	path = path or misc.get_current_proj_path()
+	self.cache[path] = graph
+end
+
+---Find a graph in the database using path.
+---If not found, add a new graph to the database.
+---@param path path Path to load and save the graph.
+---@param log_level? string Logger log level of the graph.
+---@param show_log_in_nvim? boolean Show log in Neovim when added.
+---@param save_path? string Path to load and save the graph.
+function Database:find_graph(path, log_level, show_log_in_nvim, save_path)
+	if not self.cache[path] then
+		self:add_graph(path, log_level, show_log_in_nvim, save_path)
+	end
+
+	return self.cache[path]
+end
+
+return {
+	["Database"] = Database,
+}
