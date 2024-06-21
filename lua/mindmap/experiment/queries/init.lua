04b7794c7c9e986fa6ca92e3e99629d78e9a7a8d
Jinpeng Shi <login@jpshi.me>
2024-06-21 21:27:05 +0800

    feat(exp.query.norg): load

--- lua/mindmap/experiment/queries/init.lua
+++ lua/mindmap/experiment/queries/init.lua
@@ -1,2 +1,17 @@
-local current_folder = vim.fn.expand("%:p:h")
-vim.cmd("set runtimepath+=" .. current_folder)
+vim.opt.runtimepath:append(vim.fn.expand("%:p:h:h"))
+
+local files = vim.api.nvim_get_runtime_file("queries/norg/highlights.scm", true)
+
+local function load_query(language, filename)
+	local file = assert(io.open(filename, "r"), "Failed to open file: " .. filename)
+	local contents = file:read("*a")
+	file:close()
+
+	vim.treesitter.query.set(language, "highlights", contents)
+end
+
+for _, v in pairs(files) do
+	-- print(v)
+
+	load_query("norg", v)
+end
