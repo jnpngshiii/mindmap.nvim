1398a97d28c603ebc2cc176119b9a770968e65a5
Jinpeng Shi <login@jpshi.me>
2024-06-24 08:23:38 +0800

    feat(edge): `BaseEdge` v10 -> v12

--- lua/mindmap/edge/BaseEdge.lua
+++ lua/mindmap/edge/BaseEdge.lua
@@ -7,28 +7,27 @@
 
 ---@class BaseEdge
 ---Mandatory fields:
----@field id EdgeID ID of the edge.
----@field from_node_id NodeID Where this edge is from.
----@field to_node_id NodeID Where this edge is to.
+---@field _type EdgeType Type of the edge.
+---@field _id EdgeID ID of the edge.
+---@field _from NodeID ID of the node where the edge is from.
+---@field _to NodeID ID of the node where the edge is to.
 ---Optional fields:
----@field data table Data of the node. Subclass should put there own field in this field.
----@field type EdgeType Type of the edge. Auto generated.
----@field tag table<string, string> Tag of the edge. Experimental.
----@field state string State of the edge. Default to "active". Can be "active", "removed", and "archived". Experimental.
----@field version integer Version of the edge. Auto generated and updated. Experimental.
----@field created_at integer Created time of the edge in UNIX timestemp format. Auto generated.
----@field updated_at integer Updated time of the edge in UNIX timestemp format. Used in space repetition. Auto generated and updated.
----@field due_at integer Due time of the edge in UNIX timestemp format. Used in space repetition. Auto generated and updated.
----@field ease integer Ease of the edge. Used in space repetition. Auto generated and updated.
----@field interval integer Interval of the edge. Used in space repetition. Auto generated and updated.
----@field answer_count integer Count of answer of the edge. Used in space repetition. Auto generated and updated.
----@field ease_count integer Count of answer of the edge. Used in space repetition. Auto generated and updated.
----@field again_count integer Count of answer of the edge. Used in space repetition. Auto generated and updated.
----@field cache table Cache of the edge. Save temporary data to avoid recalculation. Auto generated and updated.
+---@field _data table Data of the node. Subclass should put there own field in this field.
+---@field _cache table Cache of the edge.
+---@field _created_at integer Created time of the edge in UNIX timestemp format.
+---@field _updated_at integer Updated time of the edge in UNIX timestemp format.
+---@field _due_at integer Due time of the edge in UNIX timestemp format.
+---@field _ease integer Ease of the edge.
+---@field _interval integer Interval of the edge.
+---@field _answer_count integer Count of total answer of the edge.
+---@field _ease_count integer Count of esae answer of the edge.
+---@field _again_count integer Count of again answer of the edge.
+---@field _state string State of the edge. Can be "active", "removed", and "archived". Default: "active".
+---@field _version integer Version of the edge.
 local BaseEdge = {}
 BaseEdge.__index = BaseEdge
 
-local base_edge_version = 10
+local base_edge_version = 12
 -- v0: Initial version.
 -- v1: Add `tag` field.
 -- v2: Remove `id` field.
@@ -40,68 +39,69 @@ local base_edge_version = 10
 -- v8: Add `[before|after]_[add_into|remove_from]_graph` methods.
 -- v9: Add `ease_count` field.
 -- v10: Rename to `BaseEdge`.
+-- v11: Remove `tag` field.
+-- v12: Rename `field` to `_field`.
 
 ----------
 -- Basic Method
 ----------
 
 ---Create a new edge.
----@param id EdgeID ID of the edge.
----@param from_node_id NodeID Where this edge is from.
----@param to_node_id NodeID Where this edge is to.
----
----@param data? table Data of the edge.
----@param type? EdgeType Type of the edge.
----@param tag? table<string, string> Tag of the edge.
----@param state? string State of the edge. Default to "active". Can be "active", "removed", and "archived".
----@param version? integer Version of the edge.
----@param created_at? integer Created time of the edge.
----@param updated_at? integer Updated time of the edge.
----@param due_at? integer Due time of the edge.
----@param ease? integer Ease of the edge.
----@param interval? integer Interval of the edge.
----@param answer_count? integer Count of answer of the edge.
----@param ease_count? integer Count of answer of the edge.
----@param again_count? integer Count of answer of the edge.
+---@param _type EdgeType Type of the edge.
+---@param _id EdgeID ID of the edge.
+---@param _from NodeID ID of the node where the edge is from.
+---@param _to NodeID ID of the node where the edge is to.
+---Optional fields:
+---@param _data table Data of the node. Subclass should put there own field in this field.
+---@param _cache table Cache of the edge.
+---@param _created_at integer Created time of the edge in UNIX timestemp format.
+---@param _updated_at integer Updated time of the edge in UNIX timestemp format.
+---@param _due_at integer Due time of the edge in UNIX timestemp format.
+---@param _ease integer Ease of the edge.
+---@param _interval integer Interval of the edge.
+---@param _answer_count integer Count of total answer of the edge.
+---@param _ease_count integer Count of esae answer of the edge.
+---@param _again_count integer Count of again answer of the edge.
+---@param _state string State of the edge. Can be "active", "removed", and "archived". Default: "active".
+---@param _version integer Version of the edge.
 ---@return BaseEdge _ The created edge.
 function BaseEdge:new(
-	id,
-	from_node_id,
-	to_node_id,
+	_type,
+	_id,
+	_from,
+	_to,
 	--
-	data,
-	type,
-	tag,
-	state,
-	version,
-	created_at,
-	updated_at,
-	due_at,
-	ease,
-	interval,
-	answer_count,
-	ease_count,
-	again_count
+	_data,
+	_cache,
+	_created_at,
+	_updated_at,
+	_due_at,
+	_ease,
+	_interval,
+	_answer_count,
+	_ease_count,
+	_again_count,
+	_state,
+	_version
 )
 	local base_edge = {
-		id = id,
-		from_node_id = from_node_id,
-		to_node_id = to_node_id,
+		_type = _type,
+		_id = _id,
+		_from = _from,
+		_to = _to,
 		--
-		data = data or {},
-		type = type or "BaseEdge",
-		tag = tag or {},
-		state = state or "active",
-		version = version or base_edge_version, -- TODO: add merge function
-		created_at = created_at or tonumber(os.time()),
-		updated_at = updated_at or tonumber(os.time()),
-		due_at = due_at or 0,
-		ease = ease or 250,
-		interval = interval or 1,
-		answer_count = answer_count or 0,
-		ease_count = ease_count or 0,
-		again_count = again_count or 0,
-		cache = {},
+		_data = _data or {},
+		_cache = _cache or {},
+		_created_at = _created_at or tonumber(os.time()),
+		_updated_at = _updated_at or tonumber(os.time()),
+		_due_at = _due_at or 0,
+		_ease = _ease or 250,
+		_interval = _interval or 1,
+		_answer_count = _answer_count or 0,
+		_ease_count = _ease_count or 0,
+		_again_count = _again_count or 0,
+		_state = _state or "active",
+		_version = _version or base_edge_version,
 	}
 	base_edge.__index = base_edge
 	setmetatable(base_edge, BaseEdge)
@@ -119,7 +119,7 @@ end
 ---@return nil _ This function does not return anything.
 ---@diagnostic disable-next-line: unused-vararg
 function BaseEdge:before_add_into_graph(...)
-	-- error("[BaseEdge] Please implement function `before_add_into_graph` in subclass.")
+	vim.notify("[BaseEdge] Method `before_add_into_graph` is not implemented.")
 end
 
 ---@abstract
@@ -127,7 +127,7 @@ end
 ---@return nil _ This function does not return anything.
 ---@diagnostic disable-next-line: unused-vararg
 function BaseEdge:after_add_into_graph(...)
-	-- error("[BaseEdge] Please implement function `after_add_into_graph` in subclass.")
+	vim.notify("[BaseEdge] Method `after_add_into_graph` is not implemented.")
 end
 
 ---@abstract
@@ -135,7 +135,7 @@ end
 ---@return nil _ This function does not return anything.
 ---@diagnostic disable-next-line: unused-vararg
 function BaseEdge:before_remove_from_graph(...)
-	-- error("[BaseEdge] Please implement function `before_remove_from_graph` in subclass.")
+	vim.notify("[BaseEdge] Method `before_remove_from_graph` is not implemented.")
 end
 
 ---@abstract
@@ -143,7 +143,7 @@ end
 ---@return nil _ This function does not return anything.
 ---@diagnostic disable-next-line: unused-vararg
 function BaseEdge:after_remove_from_graph(...)
-	-- error("[BaseEdge] Please implement function `after_remove_from_graph` in subclass.")
+	vim.notify("[BaseEdge] Method `after_remove_from_graph` is not implemented.")
 end
 
 --------------------
