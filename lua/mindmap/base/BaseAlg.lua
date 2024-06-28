9ce4af2bd2735944f59423d7220016b80c700193
Jinpeng Shi <login@jpshi.me>
2024-06-28 14:48:39 +0800

    feat(alg): add methods `check_health` and `upgrade`

--- lua/mindmap/base/BaseAlg.lua
+++ lua/mindmap/base/BaseAlg.lua
@@ -15,10 +15,11 @@ MINUTE_SECONDS = 60
 local BaseAlg = {}
 BaseAlg.__index = BaseAlg
 
-local base_alg_version = 2
+local base_alg_version = 3
 -- v0: Initial version.
 -- v1: Rename to `BaseAlg`.
 -- v2: Remove `data` field.
+-- v3: Add `check_health` method an `upgrade` method.
 
 ----------
 -- Basic Method
@@ -26,7 +27,7 @@ local base_alg_version = 2
 
 ---Create a new algorithm.
 ---@param version? integer Version of the algorithm.
----@return BaseAlg base_alg The created algorithm.
+---@return BaseAlg? base_alg The created algorithm, or nil if check health failed.
 function BaseAlg:new(version)
 	local base_alg = {
 		initial_ease = 250,
@@ -36,7 +37,81 @@ function BaseAlg:new(version)
 	base_alg.__index = base_alg
 	setmetatable(base_alg, BaseAlg)
 
+	local success = base_alg:upgrade()
+	if not success then
+		vim.notify("[Base.Alg] Failed to upgrade alg. Return `nil`", vim.log.levels.WARN)
+		return nil
+	end
+
+	if base_alg.check_health then
+		local issues = base_alg:check_health()
+		if #issues > 0 then
+			vim.notify(
+				"[Base.Alg] Health check failed: \n" .. table.concat(issues, "\n") .. "\nReturn `nil`.",
+				vim.log.levels.WARN
+			)
+			return nil
+		end
+	end
+
 	return base_alg
+end
+
+---Upgrade the alg to the latest version.
+---To support version upgrades, implement functions named `upgrade_to_vX`
+---where `X` is the version to upgrade to. Each function should only upgrade
+---the alg by one version.
+---Example:
+---  ```lua
+---  function BaseAlg:upgrade_to_v11(self)
+---    self._new_field = "default_value"
+---    return true
+---  end
+---  ```
+---For multi-version upgrades (e.g., v8 to v11), this function will
+---sequentially call the appropriate upgrade functions (v8 to v9,
+---v9 to v10, v10 to v11) in order. If an intermediate upgrade
+---function is missing, the version number will be forcibly updated
+---without any changes to the alg's data.
+---@return boolean success Whether the upgrade was successful.
+function BaseAlg:upgrade()
+	local current_version = self._version
+	local latest_version = base_alg_version
+
+	while current_version < latest_version do
+		local next_version = current_version + 1
+		local upgrade_func = self["upgrade_to_v" .. next_version]
+		if upgrade_func then
+			local success = upgrade_func(self)
+			if not success then
+				vim.notify("[Base.Alg] Failed to upgrade to `v" .. next_version .. ".`")
+				return false
+			end
+		else
+			vim.notify("[Base.Alg] Forced upgrade to `v" .. next_version .. ".`")
+		end
+
+		current_version = next_version
+		self._version = current_version
+	end
+
+	return true
+end
+
+---Basic health check for alg.
+---Subclasses should override this method.
+---@return string[] issues List of issues. Empty if the alg is healthy.
+function BaseAlg:check_health()
+	local issues = {}
+
+	if type(self.initial_ease) ~= "number" then
+		table.insert(issues, "Invalid `initial_ease`: expected `number`, got `" .. type(self.initial_ease) .. "`;")
+	end
+	if type(self.initial_interval) ~= "number" then
+		table.insert(issues, "Invalid `initial_ease`: expected `number`, got `" .. type(self.initial_interval) .. "`;")
+	end
+
+	return issues
 end
 
 ---@abstract
