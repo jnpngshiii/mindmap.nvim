d323cc7356e023ecf449cb45c35a6d5bb05af8d2
Jinpeng Shi <login@jpshi.me>
2024-06-28 10:51:31 +0800

    ci: update

---
+++ tests/utils_spec.lua
@@ -0,0 +1,17 @@
+local utils = require("../lua/mindmap/utils")
+
+describe("utils", function()
+	describe("get_abs_path", function()
+		it("converts relative path to absolute path", function()
+			local result = utils.get_abs_path("../a/b", "/c/d")
+			assert.are.equal("/c/a/b", result)
+		end)
+	end)
+
+	describe("get_rel_path", function()
+		it("converts absolute path to relative path", function()
+			local result = utils.get_rel_path("/a/b/c", "/a/b/d")
+			assert.are.equal("../c", result)
+		end)
+	end)
+end)
